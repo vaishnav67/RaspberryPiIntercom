d1504f70f980d92ffaf4b686e2e78bef7e516f9e
Vaishnav Santhosh <vaihome@hotmail.com>
2021-05-28 14:11:56 +0530

    Android App Done

--- RaspPiIntercom/app/src/main/java/com/example/rasppiintercom/MainActivity.java
+++ RaspPiIntercom/app/src/main/java/com/example/rasppiintercom/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.rasppiintercom;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebView;
@@ -11,7 +10,6 @@ import android.widget.EditText;
 import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,16 +26,33 @@ public class MainActivity extends AppCompatActivity {
     intent.putExtra(EXTRA_MESSAGE, message);
     startActivity(intent);
     */
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String ip = editText.getText().toString();
-        boolean ip_ad = Pattern.matches("(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}:[0-9]{4}",ip);
+        EditText editText1 = (EditText) findViewById(R.id.editText);
+        EditText editText2 = (EditText) findViewById(R.id.editTextMotion);
+        String ip = editText1.getText().toString();
+        String motion = editText2.getText().toString();
+        boolean ip_ad = Pattern.matches("(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}",ip);
         if(ip_ad==false) {
             Snackbar mySnackbar = Snackbar.make(view, "Not an IP address", BaseTransientBottomBar.LENGTH_LONG);
             mySnackbar.show();
         }
         else {
             WebView mWebView = (WebView) findViewById(R.id.camView);
-            mWebView.loadUrl("http://"+ip);
+            mWebView.loadUrl("http://"+ip+motion);
+        }
+    }
+    public void unlockDoor(View view){
+        EditText editText1 = (EditText) findViewById(R.id.editText);
+        EditText editText2 = (EditText) findViewById(R.id.editTextFlask);
+        String ip = editText1.getText().toString();
+        String flask = editText2.getText().toString();
+        boolean ip_ad = Pattern.matches("(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}",ip);
+        if(ip_ad==false) {
+            Snackbar mySnackbar = Snackbar.make(view, "Not an IP address", BaseTransientBottomBar.LENGTH_LONG);
+            mySnackbar.show();
+        }
+        else {
+            WebView mWebView = (WebView) findViewById(R.id.camView);
+            mWebView.loadUrl("http://"+ip+":"+flask+"/unlock");
         }
     }
 }
